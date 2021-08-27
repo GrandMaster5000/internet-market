dbb1e07e2c202703415c292a0f5d0a1b9413f569
Tolik22869 <ggamer556@mail.ru>
2021-08-27 10:49:49 +0300

    add basket page

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Switch, Route } from 'react-router';
 import Phone from './containers/phone';
 import Phones from './containers/phones';
+import Basket from './containers/basket';
 
 export default (
     <Switch>
         <Route path='/' exact component={Phones}/>
+        <Route path='/basket' component={Basket}/>
         <Route path='/phones/:id' component={Phone}/>
         <Route path='/categories/:id' component={Phones}/>
     </Switch>
